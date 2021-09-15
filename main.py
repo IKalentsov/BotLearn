fe2f1c8fb5bc497b6edad855f5e3dfa7c3c3b90e
ProjectHW <ya.ko4erapbuhunyx@yandex.ru>
2021-09-15 23:12:51 +0300

    added some answers for the bot

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import discord
 import os
+import requests
+import json
+import random
+
 from dotenv import load_dotenv
 
 client = discord.Client()
+
+sad_words = ["sad", "depressed", "unhappy", "angry", "miserable"]
+
+starter_encouragements = [
+    "Cheer up!",
+    "Hang in there.",
+    "You are a great person / bot!"
+]
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = json_data[0]['q'] + " -" + json_data[0]['a']
+    return quote
 
 
 @client.event
@@ -14,9 +33,24 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    msg = message.content
 
-    if message.content.startswith('$hello'):
+    if msg.startswith('$hello'):
         await message.channel.send('Hello')
+
+    elif msg.startswith('$inspire'):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+    elif msg.startswith('nice'):
+        await message.channel.send('to meet you')
+
+    elif any(word in msg for word in sad_words):
+        await message.channel.send(random.choice(starter_encouragements))
+
+    elif msg.startswith('good'):
+        await message.channel.send('work!')
+
 
 load_dotenv('Token/Token.env')
 client.run(os.getenv('TOKEN'))
